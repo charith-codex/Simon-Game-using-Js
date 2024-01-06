1aaa436138ed458382b7d6a86a8f2c3f77253860
Charith Wickramasinghe <131009269+charith-codex@users.noreply.github.com>
2024-01-06 12:15:06 +0530

    version 1

---
+++ game.js
@@ -0,0 +1,12 @@
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+
+
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
