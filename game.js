238474b88707bc7ce415dbb3e367536b21836610
Charith Wickramasinghe <131009269+charith-codex@users.noreply.github.com>
2024-01-06 12:52:33 +0530

    ver 1.1

--- game.js
+++ game.js
@@ -1,12 +1,22 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern =[];
 
+$(".btn").click (function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    //console.log(userClickedPattern);
+})
 
-
-
-function nextSequence(){
+function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
